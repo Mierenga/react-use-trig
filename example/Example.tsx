564eba92b544d3e3a4c9e8d1c29d02a31059beba
Mierenga <mike.swierenga@gmail.com>
2021-03-02 18:10:32 -0500

    Add usage example in a functional React component.

---
+++ example/Example.tsx
@@ -0,0 +1,20 @@
+import useTrigs from 'react-use-trig';
+
+function Example(): JSX.Element {
+  const { trig } = useTrigs();
+
+  trig(setTimeout(() => console.log('pass a NodeJS.Timeout returned from setTimeout()'), 1000));
+
+  trig([
+    setTimeout(() => console.log('pass an array of NodeJS.Timeouts'), 2000),
+    setTimeout(() => console.log('pass an array of NodeJS.Timeouts'), 3000),
+  ]);
+
+  trig({ t: 4000, f: () => console.log('pass TimeoutArgs with milliseconds t and function f')});
+  trig([
+    { t: 5000, f: () => console.log('pass an array of TimeoutArgs')},
+    { t: 6000, f: () => console.log('pass an array of TimeoutArgs')},
+  ]);
+
+  return <></>;
+}
