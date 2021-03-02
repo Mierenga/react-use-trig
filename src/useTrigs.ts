73da31ba12b5ed1b6c0d4a3a7eac190df92aec7b
Mierenga <mike.swierenga@gmail.com>
2021-03-02 18:09:43 -0500

    Add initial implementation of useTrigs() hook.

---
+++ src/useTrigs.ts
@@ -0,0 +1,18 @@
+import { useEffect, useRef } from 'react';
+
+interface TrigArgs { f: () => void, t: number };
+
+export default function useTrigs() {
+  const timeouts = useRef<NodeJS.Timeout[]>([]);
+  useEffect(() => {
+    return () => timeouts.current.forEach(clearTimeout);
+  }, []);
+  return {
+    trig: (t: NodeJS.Timeout|NodeJS.Timeout[]|TrigArgs|TrigArgs[]) => {
+      const values: (NodeJS.Timeout|TrigArgs)[] = Array.isArray(t) ? t: [t];
+      const newTimeouts = values.map(v => 'f' in v ? setTimeout(v.f, v.t) : v);
+      timeouts.current.push(...newTimeouts);
+      return newTimeouts;
+    },
+  };
+}
