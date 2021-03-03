786d59d118411b6baa50b913b801ac4f3a97ecc5
Mierenga <mike.swierenga@gmail.com>
2021-03-02 22:50:01 -0500

    Wrap the returned trig function in a useCallback() hook.

--- src/useTrig.ts
+++ src/useTrig.ts
@@ -1,18 +1,22 @@
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 
 interface TrigArgs { f: () => void, t: number };
 
 export default function useTrig() {
   const timeouts = useRef<NodeJS.Timeout[]>([]);
+
   useEffect(() => {
     return () => timeouts.current.forEach(clearTimeout);
   }, []);
+
+  const trig = useCallback((t: NodeJS.Timeout|NodeJS.Timeout[]|TrigArgs|TrigArgs[]) => {
+    const values: (NodeJS.Timeout|TrigArgs)[] = Array.isArray(t) ? t: [t];
+    const newTimeouts = values.map(v => 'f' in v ? setTimeout(v.f, v.t) : v);
+    timeouts.current.push(...newTimeouts);
+    return newTimeouts;
+  },[]);
+
   return {
-    trig: (t: NodeJS.Timeout|NodeJS.Timeout[]|TrigArgs|TrigArgs[]) => {
-      const values: (NodeJS.Timeout|TrigArgs)[] = Array.isArray(t) ? t: [t];
-      const newTimeouts = values.map(v => 'f' in v ? setTimeout(v.f, v.t) : v);
-      timeouts.current.push(...newTimeouts);
-      return newTimeouts;
-    },
+    trig,
   };
 }
